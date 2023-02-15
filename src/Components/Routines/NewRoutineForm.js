1aa91dcf87c635756c83f0088cbe258e40ae4d47
cvmug <catherine.mugnai@gmail.com>
2023-02-15 14:34:58 -0500

    added form but getting token not found error on submit

---
+++ src/Components/Routines/NewRoutineForm.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+
+export default function NewRoutineForm() {
+    const [token, setToken] = useState('');
+    const [user, setUser] = useState(null);
+    const [name, setName] = useState('');
+    const [goal, setGoal] = useState('');
+    const [isPublic, setIsPublic] = useState(false);
+  
+    
+    useEffect(() => {
+        const token = window.localStorage.getItem('token');
+        setToken(token)
+        if (token) {
+            fetch('http://fitnesstrac-kr.herokuapp.com/api/users/me', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            })
+                .then((response) => response.json())
+                .then((result) => {
+                    const user = result.data;
+                    setUser(user);
+                    console.log(user)
+                })
+                .catch((error) => console.log(error));
+        }
+    }, [token]);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+      const token = window.localStorage.getItem('token');
+      if (!token) {
+        console.log('No token found');
+        return;
+      }
+  
+      fetch('http://fitnesstrac-kr.herokuapp.com/api/routines', {
+        method: 'POST',
+        body: JSON.stringify({
+          name,
+          goal,
+          isPublic
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => console.error(error));
+  
+      setName('');
+      setGoal('');
+      setIsPublic(false);
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={event => setName(event.target.value)} />
+        </label>
+        <br />
+        <label>
+          Goal:
+          <input type="text" value={goal} onChange={event => setGoal(event.target.value)} />
+        </label>
+        <br />
+        <label>
+          Public:
+          <input type="checkbox" checked={isPublic} onChange={event => setIsPublic(event.target.checked)} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+  
