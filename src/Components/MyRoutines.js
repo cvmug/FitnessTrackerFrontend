3f9f883f25097d1f226f0452530fbf17cd49ef7e
kabogle <kaboglejr@gmail.com>
2023-02-15 10:57:37 -0600

    added back in

--- src/Components/MyRoutines.js
+++ src/Components/MyRoutines.js
@@ -1,14 +1,39 @@
-import React from "react";
-import Header from "./Header";
+import React, { useState, useEffect } from 'react';
 
+ function PublicRoutines() {
+   const [routines, setRoutines] = useState([]);
 
-const MyRoutines = () => {
-    return (
-        <>
-            <Header />
-            <h1>MyRoutines</h1>
-        </>
-    )
-}
+   useEffect(() => {
+     fetch('http://fitnesstrac-kr.herokuapp.com/api/routines')
+       .then(response => response.json())
+       .then(data => setRoutines(data))
+       .catch(error => console.error(error));
+   }, []);
 
-export default MyRoutines;
+   return (
+     <div>
+       <h2>Public Routines</h2>
+       <ul>
+         {routines.map(routine => (
+           <li key={routine.id}>
+             <h3>{routine.name}</h3>
+             <p>Goal: {routine.goal}</p>
+             <p>Creator: {routine.creatorName}</p>
+             <ul>
+               {routine.activities.map(activity => (
+                 <li key={activity.id}>
+                   <p>{activity.name}</p>
+                   <p>{activity.description}</p>
+                   <p>Duration: {activity.duration} minutes</p>
+                   <p>Count: {activity.count}</p>
+                 </li>
+               ))}
+             </ul>
+           </li>
+         ))}
+       </ul>
+     </div>
+   );
+ }
+
+ export default PublicRoutines;
